dbb3be07498916f507e8ccd7e693210ce534e39d
roxx18 <roxx181104@gmail.com>
2024-11-21 21:48:54 +0530

    updated

--- backend/utils/jwttokens.js
+++ backend/utils/jwttokens.js
@@ -3,12 +3,21 @@ export const sendToken = (user, statusCode, res, message) => {
     const token = user.getJwtToken();
 
     // Cookie options
-    const options = {
+    // const options = {
+    //     expires: new Date(
+    //         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000 // Convert days to milliseconds
+    //     ),
+    //     httpOnly: true, // Ensures the cookie is not accessible via JavaScript on the client-side
+    // };
+
+    const options={
+        httpOnly: true, // Prevents access from JavaScript (secure)
+        secure: process.env.NODE_ENV === "production", // Ensures cookies are sent over HTTPS in production
+        sameSite: "strict", // Prevents CSRF attacks
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000 // Convert days to milliseconds
         ),
-        httpOnly: true, // Ensures the cookie is not accessible via JavaScript on the client-side
-    };
+      }
 
     // Set the token in the cookie and send the response
     res.status(statusCode).cookie("token", token, options).json({
