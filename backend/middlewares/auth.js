34a42b9464465acdfeb17f21996884111e56ab1f
roxx18 <roxx181104@gmail.com>
2024-11-21 22:21:17 +0530

    updated

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -5,11 +5,12 @@ import jwt from "jsonwebtoken";
 
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
   const { token } = req.cookies;
+  console.log(token)
   if (!token) {
     return next(new ErrorHandler("User Not Authorized", 401));
   }
   const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-
+  console.log(decoded)
   req.user = await User.findById(decoded.id);
 
   next();
--- backend/utils/jwttokens.js
+++ backend/utils/jwttokens.js
@@ -12,11 +12,9 @@ export const sendToken = (user, statusCode, res, message) => {
 
     const options={
         httpOnly: true, // Prevents access from JavaScript (secure)
-        secure: process.env.NODE_ENV === "production", // Ensures cookies are sent over HTTPS in production
+        // secure: process.env.NODE_ENV == "production", // Ensures cookies are sent over HTTPS in production
         sameSite: "strict", // Prevents CSRF attacks
-        expires: new Date(
-            Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000 // Convert days to milliseconds
-        ),
+        maxAge: 7 * 24 * 60 * 60 * 1000,
       }
 
     // Set the token in the cookie and send the response
